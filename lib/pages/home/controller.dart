c795123793e405959e809962cad7529637b3a481
bing <bzcat@foxmail.com>
2024-08-31 16:40:35 +0800

    update

--- lib/pages/home/controller.dart
+++ lib/pages/home/controller.dart
@@ -18,7 +18,7 @@ class HomeController extends GetxController {
 
   final dio = Dio(BaseOptions(//
     // baseUrl: 'http://119.45.248.52:8080/',
-    baseUrl: 'http://127.0.0.1:8080',
+    baseUrl: 'http://127.0.0.1:8080/',
     receiveDataWhenStatusError: true,
   ));
 
